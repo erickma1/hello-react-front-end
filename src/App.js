39a052416ccb2e35d7a7e29d315d81563d2625bb
erickma1 <110662611+erickma1@users.noreply.github.com>
2024-02-24 10:10:37 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom';
 import Greeting from './components/Greeting';
+import Home from './components/Home';
 
 function App() {
   return (
-    <>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Greeting />} />
-        </Routes>
-      </div>
-    </>
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-light bg-white border-bottom">
+        <div className="container">
+          <div className="collapse navbar-collapse justify-content-start" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item border-right">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-link" to="/greeting">
+                  Greetings
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/greeting" element={<Greeting />} />
+      </Routes>
+    </div>
+
   );
 }
 
