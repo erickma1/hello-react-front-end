44b13222aa185b7bdad6ca7f3e391d8b023d3878
erickma1 <110662611+erickma1@users.noreply.github.com>
2024-02-24 10:10:32 +0000

    Update Greeting.js

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,45 +1,14 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchData } from '../redux/greetings/greetingsSlice';
+import { fetchGreeting } from '../redux/greetingSlice';
 
-function Greeting() {
+export default function Greeting() {
   const dispatch = useDispatch();
-  const { greeting, isLoading, error } = useSelector((state) => state.greetings);
-
+  const greeting = useSelector((state) => state.greeting.message);
   useEffect(() => {
-    dispatch(fetchData());
-  },
-  [dispatch]);
-
-  const handleClick = () => {
-    dispatch(fetchData());
-  };
-
-  if (isLoading) {
-    return (
-      <>
-        <div className="loading">...isLoading</div>
-      </>
-    );
-  }
-
-  if (error) {
-    return (
-      <>
-        <div className="error">
-          error...
-        </div>
-      </>
-    );
-  }
-
+    dispatch(fetchGreeting());
+  }, [dispatch]);
   return (
-    <>
-      <div>Random Greeting</div>
-      <div>{greeting}</div>
-      <button type="button" onClick={handleClick}>Get new greeting</button>
-    </>
+    <h1 className="mt-2 text-center">{greeting}</h1>
   );
 }
-
-export default Greeting;
